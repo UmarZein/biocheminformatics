ea271367bf0276914376d70ccd39b693bbb3d69b
Umar Zein Ardhi Siregar <umarzein1314@gmail.com>
2025-03-05 09:33:44 +0700

    added new models

--- models/schnet_generalized.py
+++ models/schnet_generalized.py
@@ -8,19 +8,77 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-from .mlp import MLP
 from torch.nn import Embedding, Linear, ModuleList, Sequential, Sigmoid
 from torch_geometric.data import Dataset, download_url, extract_zip
 from torch_geometric.io import fs
 from torch_geometric.nn import MessagePassing, SumAggregation, radius_graph
 from torch_geometric.nn.resolver import aggregation_resolver as aggr_resolver
 from torch_geometric.typing import OptTensor
+from torch import nn
+from tqdm.auto import tqdm
+import inspect
+
+def auto_save_hyperparams(init_fn):
+    def wrapper(self, *args, **kwargs):
+        # Bind the arguments to the function signature and apply defaults
+        sig = inspect.signature(init_fn)
+        bound_args = sig.bind(self, *args, **kwargs)
+        bound_args.apply_defaults()
+        # Save all parameters except 'self'
+        self.hparams = {
+            name: value 
+            for name, value in bound_args.arguments.items() 
+            if name != "self"
+        }
+        return init_fn(self, *args, **kwargs)
+    return wrapper
+class MLP(nn.Module):
+    @auto_save_hyperparams
+    def __init__(self, 
+                input_dim, 
+                hidden_dims, 
+                output_dim, 
+                norm=nn.LayerNorm, 
+                final_norm=nn.Identity, 
+                activation=nn.SiLU, 
+                final_activation=nn.Identity, 
+                dropout_rate=0.1, 
+                final_dropout_rate=0.0,
+                ):
+        super().__init__()
+        dims=[input_dim]+hidden_dims+[output_dim]
+        self.lins=nn.ModuleList()
+        self.norms=nn.ModuleList()
+        self.acts=nn.ModuleList()
+        self.dropouts=nn.ModuleList()
+        for i in range(len(dims)-1):
+            self.lins.append(nn.Linear(dims[i], dims[i+1]))
+            if i+1<len(dims)-1:
+                self.norms.append(norm(dims[i+1]))
+                self.acts.append(activation())
+                self.dropouts.append(nn.Dropout(dropout_rate))
+            else:
+                self.norms.append(final_norm(dims[i+1]))
+                self.acts.append(final_activation())
+                self.dropouts.append(nn.Dropout(final_dropout_rate))
+        self.reset_parameters()
+    def reset_parameters(self):
+        for layer in self.lins:
+            torch.nn.init.xavier_uniform_(layer.weight)
+            layer.bias.data.fill_(0)
+    def forward(self, x):
+        for lin, norm, act, do in zip(self.lins, self.norms, self.acts, self.dropouts):
+            x=lin(x)
+            x=norm(x)
+            x=act(x)
+            x=do(x)
+        return x
 
 
 class Inner(torch.nn.Module):
 
     url = 'http://www.quantum-machine.org/datasets/trained_schnet_models.zip'
-
+    @auto_save_hyperparams
     def __init__(
         self,
         hidden_channels: int = 128,
@@ -30,6 +88,7 @@ class Inner(torch.nn.Module):
         cutoff: float = 10.0,
         max_num_neighbors: int = 32,
         readout: str = 'add',
+        train_distance_expansion=True,
     ):
         super().__init__()
 
@@ -41,6 +100,7 @@ class Inner(torch.nn.Module):
         self.sum_aggr = SumAggregation()
         self.readout = aggr_resolver(readout)
         self.scale = None
+        self.train_distance_expansion=train_distance_expansion
 
         # Support z == 0 for padding atoms so that their embedding vectors
         # are zeroed and do not receive any gradients.
@@ -48,7 +108,8 @@ class Inner(torch.nn.Module):
 
         self.interaction_graph = RadiusInteractionGraph(cutoff, max_num_neighbors)
 
-        self.distance_expansion = GaussianSmearing(0.0, cutoff, num_gaussians)
+        self.distance_expansion = MLP(1,[256], num_gaussians, final_activation=nn.Sigmoid, dropout_rate=0.0)
+        self.gaussian_smear = GaussianSmearing(0.0, cutoff, num_gaussians)
 
         self.interactions = ModuleList()
         
@@ -62,33 +123,58 @@ class Inner(torch.nn.Module):
         self.lin2 = Linear(hidden_channels // 2, 1)
 
 
-        self.reset_parameters()
+        self.reset_parameters(cutoff=cutoff)
 
-    def reset_parameters(self):
+    def reset_parameters(self,cutoff=10):
         self.embedding.reset_parameters()
+        
         for interaction in self.interactions:
             interaction.reset_parameters()
         torch.nn.init.xavier_uniform_(self.lin1.weight)
         self.lin1.bias.data.fill_(0)
         torch.nn.init.xavier_uniform_(self.lin2.weight)
         self.lin2.bias.data.fill_(0)
-        
+
+        #pretrain distance expansion
+        opt=torch.optim.AdamW(self.distance_expansion.parameters())
+        x=torch.linspace(0,cutoff,256)
+        z=(x*2-cutoff).view(-1,1)/cutoff
+        y=self.gaussian_smear(x)
+        p=self.distance_expansion(z)
+        loss=((y-p)**2).mean()
+        print('before:', loss.item())
+        it=tqdm(range(10_000))
+        for _ in it:
+            p=self.distance_expansion(z)
+            loss=((y-p)**2).mean()
+            it.set_description(str(int(loss*100000)/100000))
+            self.distance_expansion.zero_grad(set_to_none=True)
+            loss.backward()
+            opt.step()
+        print('after:', loss.item())
+    
     def forward(self, z: Tensor, pos: Tensor,
                 batch: OptTensor = None) -> Tensor:
         batch = torch.zeros_like(z) if batch is None else batch
 
         h = self.embedding(z)
-        edge_index, edge_weight = self.interaction_graph(pos, batch)
-        edge_attr = self.distance_expansion(edge_weight)
+        edge_index, bond_lengths = self.interaction_graph(pos, batch)
+        bond_lengths=((bond_lengths-2.7554)/1.1664).view(-1,1)
+        if self.train_distance_expansion:
+            edge_attr = self.distance_expansion(bond_lengths)
+        else:
+            with torch.no_grad():
+                edge_attr = self.distance_expansion(bond_lengths)
         
         for interaction in self.interactions:
-            h = h + interaction(h, edge_index, edge_weight, edge_attr)
+            h = h + interaction(h, edge_index, edge_attr)
 
+        # MLP
         h = self.lin1(h)
         h = self.act(h)
         h = self.lin2(h)
 
-
+        # SUM
         out = self.readout(h, batch, dim=0)
 
         return out
@@ -132,8 +218,8 @@ class RadiusInteractionGraph(torch.nn.Module):
         edge_index = radius_graph(pos, r=self.cutoff, batch=batch,
                                   max_num_neighbors=self.max_num_neighbors)
         row, col = edge_index
-        edge_weight = (pos[row] - pos[col]).norm(dim=-1)
-        return edge_index, edge_weight
+        bond_lengths = (pos[row] - pos[col]).norm(dim=-1)
+        return edge_index, bond_lengths
 
 
 class InteractionBlock(torch.nn.Module):
@@ -161,9 +247,9 @@ class InteractionBlock(torch.nn.Module):
         torch.nn.init.xavier_uniform_(self.lin.weight)
         self.lin.bias.data.fill_(0)
 
-    def forward(self, x: Tensor, edge_index: Tensor, edge_weight: Tensor,
+    def forward(self, x: Tensor, edge_index: Tensor,
                 edge_attr: Tensor) -> Tensor:
-        x = self.conv(x, edge_index, edge_weight, edge_attr)
+        x = self.conv(x, edge_index, edge_attr)
         x = self.act(x)
         x = self.lin(x)
         return x
@@ -191,10 +277,9 @@ class CFConv(MessagePassing):
         torch.nn.init.xavier_uniform_(self.lin2.weight)
         self.lin2.bias.data.fill_(0)
 
-    def forward(self, x: Tensor, edge_index: Tensor, edge_weight: Tensor,
+    def forward(self, x: Tensor, edge_index: Tensor,
                 edge_attr: Tensor) -> Tensor:
-        C = 0.5 * (torch.cos(edge_weight * PI / self.cutoff) + 1.0)
-        W = self.nn(edge_attr) * C.view(-1, 1)
+        W = self.nn(edge_attr)
 
         x = self.lin1(x)
         x = self.propagate(edge_index, x=x, W=W)
@@ -231,7 +316,8 @@ class ShiftedSoftplus(torch.nn.Module):
         return F.softplus(x) - self.shift
 
 
-class SchNetMinim(Inner):
+class SchNetGeneralized(Inner):
+    @auto_save_hyperparams
     def __init__(self, 
         hidden_channels: int = 128,
         num_filters: int = 128,
@@ -240,6 +326,7 @@ class SchNetMinim(Inner):
         cutoff: float = 10.0,
         max_num_neighbors: int = 32,
         readout: str = 'add',
+        train_distance_expansion=True,
                 ):
         self.__hidden_channels=hidden_channels
         self.__num_filters=num_filters
@@ -256,6 +343,7 @@ class SchNetMinim(Inner):
             cutoff=cutoff,
             max_num_neighbors=max_num_neighbors,
             readout=readout,
+            train_distance_expansion=train_distance_expansion,
         )
     @classmethod
     def from_config(cls, config):
